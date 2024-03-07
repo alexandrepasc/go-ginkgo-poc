8faa24f177d92413760fee75b56df14b2a11298b
Alexandre Pascoal <alexandre.pascoal@gmail.com>
2024-03-07 22:27:58 +0000

    feat: Add more containers to the tests

--- tests/meowfacts/meowfacts_test.go
+++ tests/meowfacts/meowfacts_test.go
@@ -35,68 +35,73 @@ var _ = Describe("Meowfacts", func() {
 			Expect(len(respBody.Data)).To(Equal(1))
 		})
 
-		Context("With parameters", func() {
-			It("Count 3", func() {
-				req, err := http.NewRequest("GET", "https://meowfacts.herokuapp.com/", http.NoBody)
-
-				Expect(err).To(BeNil())
-
-				req.Header.Add("User-Agent", "PostmanRuntime/7.28.4")
-
-				q := req.URL.Query()
-
-				q.Add("count", "3")
-
-				req.URL.RawQuery = q.Encode()
-
-				client := &http.Client{}
-
-				resp, err := client.Do(req)
-
-				Expect(err).To(BeNil())
-
-				Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-				respBody := types.GetMeowfactsResponse{}
-				json.NewDecoder(resp.Body).Decode(&respBody)
-
-				Expect(len(respBody.Data)).To(Equal(3))
+		Context("With parameter", func() {
+			When("Count is", func() {
+				It("3", func() {
+					req, err := http.NewRequest("GET", "https://meowfacts.herokuapp.com/", http.NoBody)
+	
+					Expect(err).To(BeNil())
+	
+					req.Header.Add("User-Agent", "PostmanRuntime/7.28.4")
+	
+					q := req.URL.Query()
+	
+					q.Add("count", "3")
+	
+					req.URL.RawQuery = q.Encode()
+	
+					client := &http.Client{}
+	
+					resp, err := client.Do(req)
+	
+					Expect(err).To(BeNil())
+	
+					Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	
+					respBody := types.GetMeowfactsResponse{}
+					json.NewDecoder(resp.Body).Decode(&respBody)
+	
+					Expect(len(respBody.Data)).To(Equal(3))
+				})
 			})
-
-			It("ID 1", func() {
-				req, err := http.NewRequest("GET", "https://meowfacts.herokuapp.com/", http.NoBody)
-
-				Expect(err).To(BeNil())
-
-				req.Header.Add("User-Agent", "PostmanRuntime/7.28.4")
-
-				q := req.URL.Query()
-
-				q.Add("id", "1")
-
-				req.URL.RawQuery = q.Encode()
-
-				client := &http.Client{}
-
-				resp, err := client.Do(req)
-
-				Expect(err).To(BeNil())
-
-				Expect(resp.StatusCode).To(Equal(http.StatusOK))
-
-				e := gojsonschema.NewStringLoader(data.GetMeowfactsId1)
-
-				b, err := io.ReadAll(resp.Body)
-
-				Expect(err).To(BeNil())
-
-				a := gojsonschema.NewBytesLoader(b)
-
-				res, err := gojsonschema.Validate(e, a)
-
-				Expect(err).To(BeNil())
-
-				Expect(res.Valid()).To(BeTrue())
+			
+			When("ID is", func() {
+				// Test using json schema to validate the response body
+				It("1", func() {
+					req, err := http.NewRequest("GET", "https://meowfacts.herokuapp.com/", http.NoBody)
+	
+					Expect(err).To(BeNil())
+	
+					req.Header.Add("User-Agent", "PostmanRuntime/7.28.4")
+	
+					q := req.URL.Query()
+	
+					q.Add("id", "1")
+	
+					req.URL.RawQuery = q.Encode()
+	
+					client := &http.Client{}
+	
+					resp, err := client.Do(req)
+	
+					Expect(err).To(BeNil())
+	
+					Expect(resp.StatusCode).To(Equal(http.StatusOK))
+	
+					e := gojsonschema.NewStringLoader(data.GetMeowfactsId1)
+	
+					b, err := io.ReadAll(resp.Body)
+	
+					Expect(err).To(BeNil())
+	
+					a := gojsonschema.NewBytesLoader(b)
+	
+					res, err := gojsonschema.Validate(e, a)
+	
+					Expect(err).To(BeNil())
+	
+					Expect(res.Valid()).To(BeTrue())
+				})
 			})
 		})
 	})
